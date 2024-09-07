4a2964bd609dba08af7f9ab4276550a1a555e9f5
Lois Adegbohungbe <loisadegbohungbe@gmail.com>
2024-09-07 09:29:12 +0100

    minor update to utils.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,11 +1,5 @@
 type HttpMethod = "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
 
-interface ApiRequestConfig {
-  method?: HttpMethod;
-  body?: any;
-  headers?: HeadersInit;
-}
-
 const apiRequest = async (
   url: string,
   method: HttpMethod = "GET",
